ce8d85c3ece7ccc23e9b756314b4ab8c97731a5d
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-06-21 13:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.getenv("API_HASH")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5115331277 5025877489 1202064253 1120271521").split())
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5115331277 5025877489 1202064253 1120271521 2088238603").split())
 START_STRING = """
 **🔮 Hello There, You Can Use Me To Create Awesome Logos...**
 ➤ Click /help Or The Button Below To Know How To Use Me
